f6d771d0de38ed24fb4d184b07835d785b8afb05
Rob Lehmann <lehmannrobb@gmail.com>
2022-03-18 17:53:13 +0100

    Setup/cleaned up create-react-app with redux toolkit in frontend folder

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,29 +1,7 @@
-import { useState, useEffect } from 'react'
-import MealForm from './components/MealForm'
-import MealPlan from './components/MealPlan'
-
-const LOCAL_STORAGE_KEY = process.env.REACT_APP_KEY
-
 function App() {
-  const [meals, setMeals] = useState([])
-
-  useEffect(() => {
-    const storedMeals = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-    if (storedMeals) {
-      setMeals(storedMeals)
-    }
-  }, [])
-
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(meals))
-  }, [meals])
-
-
-
   return (
-    <div className="App">
-      <MealForm meals={meals} setMeals={setMeals} />
-      <MealPlan meals={meals} setMeals={setMeals} />
+    <div>
+      <h1>My App</h1>
     </div>
   );
 }
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,14 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import reportWebVitals from './reportWebVitals';
+import { store } from './app/store';
+import { Provider } from 'react-redux';
+import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-reportWebVitals();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
