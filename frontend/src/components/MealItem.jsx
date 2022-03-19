e14911fcfb4484967a55256cb9762fc9a24b122a
Rob Lehmann <lehmannrobb@gmail.com>
2022-03-19 15:02:46 +0300

    Update README.md

--- frontend/src/components/MealItem.jsx
+++ frontend/src/components/MealItem.jsx
@@ -1,15 +1,18 @@
 import { useDispatch } from 'react-redux'
 import { deleteMeal } from '../features/meals/mealSlice'
+import { FaMinusCircle } from 'react-icons/fa'
 
 const MealItem = ({ meal }) => {
 
     const dispatch = useDispatch()
-    
+
   return (
     <div className="meal">
         <div>{meal.day}</div>
         <h2>{meal.name}</h2>
-        <button onClick={() => dispatch(deleteMeal(meal._id))} className="close">x</button>
+        <button onClick={() => dispatch(deleteMeal(meal._id))} className="close">
+            <FaMinusCircle />
+        </button>
     </div>
   )
 }
